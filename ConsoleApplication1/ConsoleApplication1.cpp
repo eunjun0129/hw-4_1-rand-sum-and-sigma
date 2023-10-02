37bff890c2bac0aeee59b260583830907938ecd6
eunjun <eunjun@DESKTOP-5B870T5>
2023-10-03 03:45:46 +0900

    초기 커밋.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <random>
+using namespace std;
+int main() {
+    const int arraySize = 20;
+    int x[arraySize];
+    int sum = 0;
+    int sigma = 0;
+    for (int i = 0; i < arraySize; ++i) {
+        x[i] = rand() % 100;
+    }
+
+    for (int i = 0; i < arraySize; ++i) {
+        cout << x[i] << " ";
+    }
+    for (int i = 0; i < arraySize; ++i) {
+        sum += x[i];
+    }
+    cout << "\n" << "평균은" << sum / arraySize << endl;
+    for (int i = 0; i < arraySize; ++i) {
+        int sigma = (x[i] - sum / arraySize);
+    }
+    cout <<  "표준편차는" << sigma / (arraySize-1) << endl;
+    return 0;
+}
